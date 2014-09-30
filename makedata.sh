4702f40d044d71eaf5ee4fa51c66d249d221ff2e
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2014-09-30 08:01:34 +0000

    autogenerated by https://github.com/spyysalo/jekyll-hook

--- makedata.sh
+++ makedata.sh
@@ -6,12 +6,12 @@ set -e
 DATA_DIRECTORY="_data"
 RELATION_DATA_FILE="$DATA_DIRECTORY/relations.yaml"
 
-LANGUAGES="en-dep fi-dep u-dep"
+COLLECTIONS="en-dep fi-dep u-dep"
 
-# directory names are language names with an underscore prefix.
-DIRECTORIES=$(echo " $LANGUAGES" | perl -pe 's/ (\S)/ _$1/g')
+# directory names are collection names with an underscore prefix.
+DIRECTORIES=$(echo " $COLLECTIONS" | perl -pe 's/ (\S)/ _$1/g')
 
-# unique relation (document) names from per-language directories
+# unique relation (document) names from per-collection directories
 RELATIONS=$(find $DIRECTORIES -name '*.md' -printf '%f\n' | 
     perl -pe 's/\.md$//' | sort | uniq)
 
@@ -21,17 +21,21 @@ if [ -e $DATA_DIRECTORY ]; then
 fi
 mkdir $DATA_DIRECTORY
 
-# generate YAML with relations and languages
+# generate YAML with relations and collections
 for r in $RELATIONS; do 
     echo "- label: '$r'"
+    # (not really "languages", but close enough here)
     echo "  languages:"
-    for l in $LANGUAGES; do
+    for l in $COLLECTIONS; do
 	if [ -e "_$l/$r.md" ]; then
-	    echo "  - label: '$l'"
+	    # collection directory name vs. permalink variance (see
+	    # https://github.com/UniversalDependencies/docs/issues/57)
+	    p=`echo "$l" | perl -pe 's/-(pos|feat|dep)$/\/$1/'`
+	    echo "  - label: '$p'"
 	fi
     done
 done > $RELATION_DATA_FILE
 
 echo "Found" \
     $(echo "$RELATIONS" | wc -w) "relations in" \
-    $(echo "$LANGUAGES" | wc -w) "languages" >&2
+    $(echo "$COLLECTIONS" | wc -w) "collections" >&2
