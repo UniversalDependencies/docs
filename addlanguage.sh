5d320e6f2d9b83ba268d129f4e618c138ac05bf8
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2014-10-06 13:35:39 +0000

    autogenerated by https://github.com/spyysalo/jekyll-hook

--- addlanguage.sh
+++ addlanguage.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+### WARNING: this is a brittle and poorly tested script that makes
+### bulk additions to the repository. Don't run this unless you're
+### sure what you're doing and know how to clean up the repo if
+### anything goes wrong.
+
 set -u
 set -e
 
