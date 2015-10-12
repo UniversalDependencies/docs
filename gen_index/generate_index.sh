d17ebff6246dc036a2bb24d202efb8d7c5fa843d
Filip Ginter <ginter@cs.utu.fi>
2015-10-12 20:53:08 +0300

    index.md

--- gen_index/generate_index.sh
+++ gen_index/generate_index.sh
@@ -4,4 +4,4 @@ export UD_TOOLS=$HOME/UD/tools
 
 #This assumes that refresh_corpus_data.sh did run
 
-python index_page.py --ud-data=$UD_DATA > ../index_experimental.html
+python index_page.py --ud-data=$UD_DATA > ../_includes/at_glance.html
