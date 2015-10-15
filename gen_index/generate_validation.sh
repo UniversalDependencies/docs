3edfff382339489dc9b9842fc48d97390c90cdc2
Filip Ginter <ginter@cs.utu.fi>
2015-10-15 11:00:17 +0300

    autogenerated by https://github.com/spyysalo/jekyll-hook

--- gen_index/generate_validation.sh
+++ gen_index/generate_validation.sh
@@ -1,4 +1,4 @@
-for L in ../../UD_*
+for L in $HOME/UD/UD_*
 do
     echo $L
     cd $L
@@ -6,4 +6,5 @@ do
     cd -
 done
 python validation_run.py --ud-d ~/UD --ud-t ~/UD/tools > ../_includes/validation_results.html
-git add ../_includes/validation_results.html && git commit -m "Validation auto-run" && git push
+echo "updated: $(date -R)" > ../_data/validation.yaml
+git pull && git add ../_includes/validation_results.html ../_data/validation.yaml && git commit -m "Validation auto-run" && git push
