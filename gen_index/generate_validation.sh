eb57db07e9f65e7cb54efb64f31ff8a9cb072d2d
Filip Ginter <ginter@cs.utu.fi>
2015-10-30 09:58:04 +0200

    Don't pull corpora in generate validation, I moved this to the cronjob script

--- gen_index/generate_validation.sh
+++ gen_index/generate_validation.sh
@@ -1,10 +1,3 @@
-for L in $HOME/UD/UD_* $HOME/UD/tools
-do
-    echo $L
-    cd $L
-    git pull 
-    cd -
-done
 python validation_run.py --ud-d ~/UD --ud-t ~/UD/tools > ../_includes/validation_results.html
 echo "updated: $(date -R)" > ../_data/validation.yaml
 git pull && git add ../_includes/validation_results.html ../_data/validation.yaml && git commit -m "Validation auto-run" && git push
