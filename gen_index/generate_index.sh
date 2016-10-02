32ebe85f7e95d8a324d8d114e35fd51100212a42
Dan Zeman <zeman@ufal.mff.cuni.cz>
2016-10-02 11:58:08 +0200

    Fix a json problem.

--- gen_index/generate_index.sh
+++ gen_index/generate_index.sh
@@ -7,6 +7,9 @@ export UD_TOOLS=$HOME/UD_PROJHOOK/tools
 
 #This assumes that refresh_corpus_data.sh did run
 
-python index_page.py --ud-data=$UD_DATA > ../_includes/at_glance.html
+# index_page.py has side effect of modifying ../_data/ldata.json!
+# Any subsets must be generated before the main list so that ldata.json contains the full list of treebanks.
+# Ideally, the subset scripts should be merged with index_page.py and the subset should be selected by command-line options.
 python index_page_slavic.py --ud-data=$UD_DATA > ../_includes/at_glance_slavic.html
+python index_page.py --ud-data=$UD_DATA > ../_includes/at_glance.html
 git add ../_includes/at_glance.html ../_data/ldata.json && git commit -m "Main page table regenerated (auto)"
