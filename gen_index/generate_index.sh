fe282b0f4543ddb811c1f24f5c516af98a889477
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2016-12-01 15:23:20 +0000

    git add at_glance_empty.html when run

--- gen_index/generate_index.sh
+++ gen_index/generate_index.sh
@@ -1,6 +1,6 @@
 #Where's the UD data?
-export UD_DATA=$HOME/UD_PROJHOOK/UD-dev-branches
-export UD_TOOLS=$HOME/UD_PROJHOOK/tools
+export UD_DATA=$HOME/git_checkout/universaldependencies
+export UD_TOOLS=$HOME/git_checkout/universaldependencies-tools
 
 #export UD_DATA=$HOME/UD
 #export UD_TOOLS=$HOME/UD/tools
@@ -13,4 +13,4 @@ export UD_TOOLS=$HOME/UD_PROJHOOK/tools
 python index_page_slavic.py --ud-data=$UD_DATA > ../_includes/at_glance_slavic.html
 python index_page.py --ud-data=$UD_DATA > ../_includes/at_glance.html
 python index_page.py --ud-data=$UD_DATA --empty > ../_includes/at_glance_empty.html
-git add ../_includes/at_glance.html ../_includes/at_glance_slavic.html ../_data/ldata.json && git commit -m "Main page table regenerated (auto)"
+git add ../_includes/at_glance.html ../_includes/at_glance_empty.html ../_includes/at_glance_slavic.html ../_data/ldata.json && git commit -m "Main page tables regenerated (auto)"
