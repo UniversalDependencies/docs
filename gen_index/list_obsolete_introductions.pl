5d792f93c85241ed2f272b92c00cfbfe4409cb3a
Dan Zeman <zeman@ufal.mff.cuni.cz>
2017-11-05 22:07:51 +0100

    Print the list.

--- gen_index/list_obsolete_introductions.pl
+++ gen_index/list_obsolete_introductions.pl
@@ -14,3 +14,33 @@ my @introductions = glob("./_*/introduction.md");
 print(join(', ', @introductions), "\n");
 my $n = scalar(@introductions);
 print("Total $n introductions.\n");
+print <<EOF
+---
+layout: base
+title:  'Obsolete Introductions'
+udver: '2'
+---
+
+# Obsolete Introductions
+
+This site currently contains $n language-specific introduction pages. They are
+probably obsolete and will be removed because they probably contain descriptions
+of individual treebanks (rather than languages). In the future, such information
+will be copied automatically from the README files in the treebank repositories,
+and it will be displayed per treebank, not per language. If you currently have
+more information here than in your README file, please copy it to the README
+file.
+
+EOF
+;
+foreach my $i (@introductions)
+{
+    $i =~ s:^(\./)?_::;
+    $i =~ s/\.md$/.html/;
+    my $text = $i;
+    if($text =~ m:^(.+?)/:)
+    {
+        $text = $1;
+    }
+    print("* [$text]($i)\n");
+}
